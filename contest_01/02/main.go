1055e6c7119575ecb56d7b2faa5a84a21e375dc2
Jeys Li <145220956+PolinaGvozdkova@users.noreply.github.com>
2023-10-01 23:32:34 +0300

    main.go

--- contest_01/02/main.go
+++ contest_01/02/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func main(){
-    // Код
+   human := 0.5 * 365
+   tree1 := mathCeil(human/32)
+   tree2 := mathCeil(human/20)
+   fmt.Println(human, tree1, tree2)
 }
