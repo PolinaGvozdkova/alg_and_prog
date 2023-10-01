43ebf144e0c26c416c927f4f13ff8499b291c124
Jeys Li <145220956+PolinaGvozdkova@users.noreply.github.com>
2023-10-01 23:28:34 +0300

    main.cpp

--- contest_01/01/main.cpp
+++ contest_01/01/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <cmath>
 
-int main(){
-	// Код
+int main() {
+    double pi_approximation = sqrt(12.0) * (
+        1.0 - 1.0 / (3 * 3) + 1.0 / (5 * 3 * 2) - 1.0 / (7 * 3 * 3) + 1.0 / (9 * 3 * 4) - 1.0 / (11 * 3 * 5)
+    );
+
+    std::cout << pi_approximation << std::endl;
+    
+    return 0;
 }
